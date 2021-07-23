94deaa9691bc092d1e52e6a339c0cfc186b3a6b5
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-07-23 23:25:35 +0900

    ユーザー管理機能
    ユーザー管理機能

---
+++ db/migrate/20210723115801_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false,
+      t.text :explain, null: false,
+      t.integer :category_id, null: false,
+      t.integer :status_id, null: false,
+      t.integer :delivery_charge_id, null: false,
+      t.integer :until_shipping_id, null: false,
+      t.integer :price, null: false,
+      t.integer :prefecture_id, null: false,
+
+      t.timestamps
+ belonges_to :user
+has_one :purchase
+    end
+  end
+end
---
+++ db/migrate/20210723120947_create_shipping_addresses.rb
@@ -0,0 +1,16 @@
+class CreateShippingAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shipping_addresses do |t|
+      t.references :purchase  null: false, foreign_key: true|
+      t.string :postal_code  null: false
+      t.integer :prefecture_id  null: false,    
+      t.string  :municipalities  null: false, 
+      t.string :address  null: false, 
+      t.string :building  
+      t.string :building  null: false, 
+      t.timestamps
+
+      belonges_to :purchase
+    end
+  end
+end
