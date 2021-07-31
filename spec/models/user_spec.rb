4719afa4b94ae327e0ceed64078e32861aa68e34
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-07-31 20:52:52 +0900

    ユーザー管理機能修正１
    ユーザー管理機能修正１

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,7 @@ RSpec.describe User, type: :model do
 it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
   expect(@user).to be_valid
 end
-it 'nicknameが6文字以下であれば登録できる' do
-  @user.password = '000000'
-  expect(@user).to be_valid
 
-end
-it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-  @user.password_confirmation = '000000'
-  expect(@user).to be_valid
-end
 it 'nicknameが空では登録できない' do
   @user.nickname = ''
   @user.valid?
@@ -29,19 +21,48 @@ it 'emailが空では登録できない' do
   @user.valid?
   expect(@user.errors.full_messages).to include "Email can't be blank"
 end
-it 'passwordは、６文字以上での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
-  @user.password = 'testtest'
-  @user.password = "３７S"
+it 'passwordは、5文字以下では登録出来ない' do
+  @user.password = 'tespt'
   @user.valid?
   expect(@user.errors.full_messages).to include "Email can't be blank"
 end
-it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+it 'passwordは、全角英字は登録出来ない' do
+  @user.password = 'ｔａ'
+  @user.valid?
+  expect(@user.errors.full_messages).to include "Email can't be blank"
 end
-it 'nicknameが7文字以上では登録できない' do
+
+it 'passwordは、全角数字は登録出来ない' do
+  @user.password = '０４１４'
+  @user.valid?
+  expect(@user.errors.full_messages).to include "Email can't be blank"
 end
-it '重複したemailが存在する場合登録できない' do
+
+
+it 'password_confirmationが空では登録できない' do
+  @user.password_confirmation = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include "password_confirmation can't be blank"
+
 end
-it 'passwordが5文字以下では登録できない' do
+
+it 'nicknameが7文字未満では登録できない' do
+  @user.nickname = 'タツロウ'
+  @user.valid?
+  expect(@user.errors.full_messages).to include "nickname can't be blank"
+
 end
+it '新規のemailが存在する場合登録できない' do
+  @user.email = 'shimada@14'
+  @user.valid?
+  expect(@user.errors.full_messages).to include "Email can't be blank"
+end
+
+end
+it 'passwordが4文字以下では登録できない' do
+  @user.password = 'test'
+  @user.valid?
+  expect(@user.errors.full_messages).to include "Email can't be blank"
+  
 end
 end
