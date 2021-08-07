134f9f29092b4216b60e2ccb6f8be7b98aba86bb
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-08-07 16:10:50 +0900

    価格、販売手数料、利益
    価格、販売手数料、利益まで実装行っている

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+      const inputValue = priceInput.value;
+      console.log(inputValue);
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue*0.1)
+      const profitDom = document.getElementById("profit");
+      profitDom.innerHTML = Math.floor(inputValue-addTaxDom.innerHTML)
+
+    
+
+
+
+  })
+});
