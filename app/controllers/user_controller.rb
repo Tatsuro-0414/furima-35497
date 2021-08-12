1d72edbc2dc022932541398ab5950473629cd7fe
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-08-12 12:45:11 +0900

    商品詳細表示機能修正３
    商品詳細表示機能修正３

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,10 @@
 class UserController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
+#   def show
+#     @user = User.find(params[:id])
     
     
-  end
-end
+#   end
+# end
+# ユーザーの詳細ページは追加実装範囲でございます。
+# そのため現時点ではコメントアウトをしております。
